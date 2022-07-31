f7c24490aaf9d566c9ad1dd3bf708a23aca9d0cf
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2022-07-31 17:21:34 +0200

    Install libraries, define styles and routes, build sidebar

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Layout.scss";
+
+import Sidebar from "../Sidebar/Sidebar";
+
+function Layout() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
+
+export default Layout;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Sidebar.scss";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/logo_sub.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+function Sidebar() {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="mateusz" />
+      </Link>
+      <nav>
+        <NavLink exact="true" className="active" to={"/"}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a target="_blank" rel="noreferrer" href="">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
