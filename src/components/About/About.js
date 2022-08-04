f7f62d6d0c48e7da450ff3debbfb78441b8ef5bf
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2022-08-04 22:04:44 +0200

    Bulid Page Loader

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,6 +10,7 @@ import {
   faJsSquare,
   faGitAlt,
 } from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
 
 function About() {
   const [letterClass, setLetterClass] = useState("text-animate");
@@ -21,48 +22,51 @@ function About() {
   }, []);
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            lettersArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
-            index={15}
-          />
-        </h1>
-        <p>
-          I started my journey in 1990 Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Necessitatibus vitae ut nisi tenetur soluta
-          reprehenderit sapiente nam veritatis laboriosam dolor quam ab fuga
-          accusantium non, laudantium possimus, optio suscipit omnis.
-        </p>
-        <p>I think i'm god of programming</p>
-        <p>Esports enjoyer</p>
-      </div>
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
-          </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              lettersArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+              index={15}
+            />
+          </h1>
+          <p>
+            I started my journey in 1990 Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Necessitatibus vitae ut nisi tenetur soluta
+            reprehenderit sapiente nam veritatis laboriosam dolor quam ab fuga
+            accusantium non, laudantium possimus, optio suscipit omnis.
+          </p>
+          <p>I think i'm god of programming</p>
+          <p>Esports enjoyer</p>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+            </div>
 
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#f06529" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faAngular} color="#dd0031" />
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faAngular} color="#dd0031" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,7 @@ import LogoTitle from "../../assets/images/logo-s.png";
 import { Link } from "react-router-dom";
 import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
 import Logo from "./Logo/Logo";
+import Loader from "react-loaders";
 
 function Home() {
   const [letterClass, setLetterClass] = useState("text-animate");
@@ -33,34 +34,37 @@ function Home() {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            lettersArray={nameArray}
-            index={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            lettersArray={jobArray}
-            index={22}
-          />
-        </h1>
-        <h2>Fronted Developer</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              lettersArray={nameArray}
+              index={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              lettersArray={jobArray}
+              index={22}
+            />
+          </h1>
+          <h2>Fronted Developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 }
 
